1578460d121656d1af433dd2a73a9a5fb1ff835a
Ikadzev <ikadzev@mail.ru>
2024-02-11 13:26:50 +0700

    mini 2 DONE

--- minies/2.py
+++ minies/2.py
@@ -9,13 +9,15 @@ def algo(a, b):
         a = '0' * (length - len(a)) + a
     if len(b) != length:
         b = '0' * (length - len(b)) + b
-    # print(a, b, length)
     
     aa = a[:hlen]
     bb = a[hlen:]
     cc = b[:hlen]
     dd = b[hlen:]
+    f = int(aa)*int(cc) if len(aa) == 1 and len(cc) == 1 else algo(aa, cc)
+    s = ((int(aa)+int(bb)) * (int(cc) + int(dd))) if len(str(int(aa)+int(bb))) == 1 and len(str(int(cc) + int(dd))) == 1 else algo(int(aa)+int(bb), int(cc) + int(dd))
+    t = int(bb)*int(dd) if len(bb) == 1 and len(dd) == 1 else algo(bb, dd)
     
     return (10**length*int(aa)*int(cc) + 10**hlen*(int(aa)*int(dd) + int(bb)*int(cc)) + int(bb)*int(dd))
 
-print(algo(12, 5))
+print(algo(12, 50))
