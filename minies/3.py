c69015918f74714738751409a0e3720654f9202f
Ikadzev <ikadzev@mail.ru>
2024-02-11 16:32:30 +0700

    mini 3 clean

--- minies/3.py
+++ minies/3.py
@@ -1,13 +1,15 @@
 x = int(input('Needed number: '))
-init_n = int(input('Number of elements: '))
-lst = []
-print('Elements: ', end='')
-for _ in range(init_n):
-    lst.append(int(input()))
+lst = input('List (space-splitted): ').split()
+init_n = len(lst)
+for i in range(init_n): 
+    try:
+        lst[i] = int(lst[i])
+    except ValueError:
+        print('NaN')
+        exit(0)
 n = [0, init_n]
 while True:
     cent = (n[0] + n[1]) // 2
-    print(lst, cent, n)
     if lst[cent] != x:
         if abs(n[0]-n[1]) == 1:
             n = -1
