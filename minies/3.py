dba19f48caebf5a59605b0b18262ef4c7b6605b8
Ikadzev <ikadzev@mail.ru>
2024-02-11 13:53:24 +0700

    mini 3 DONE

--- minies/3.py
+++ minies/3.py
@@ -5,15 +5,18 @@ print('Elements: ', end='')
 for _ in range(init_n):
     lst.append(int(input()))
 n = [0, init_n]
-for _ in range(10):
+while True:
     cent = (n[0] + n[1]) // 2
-    print(lst, cent)
+    print(lst, cent, n)
     if lst[cent] != x:
+        if abs(n[0]-n[1]) == 1:
+            n = -1
+            break
         if lst[cent] > x:
             n[1] = cent
         else:
             n[0] = cent
     else:
-        n = cent+1
+        n = cent
         break
 print(n)
