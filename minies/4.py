db455bbb05543b0a98a0ddd2e2abcc4a8baeb326
Ikadzev <ikadzev@mail.ru>
2024-02-12 13:04:06 +0700

    mini 4 DONE?

--- minies/4.py
+++ minies/4.py
@@ -1,25 +1,34 @@
-x = int(input('Needed number: '))
-lst = input('List (space-splitted): ').split()
-init_n = len(lst)
-for i in range(init_n): 
-    try:
-        lst[i] = int(lst[i])
-    except ValueError:
-        print('NaN')
-        exit(0)
-n = [0, init_n]
+from subprocess import check_output as ch_out
+from os import chdir, system, getcwd as pwd
+
+iwd = pwd()
+wd = input('WD: ')
+if wd == '':
+    wd = iwd
+b = input('Bad commit: ')
+g = input('Good commit: ')
+auto = True if int(input('Manual (0) or auto (1)? ')) else False
+a_command = input('Command for auto mode: ') if auto else None
+chdir(wd)
+lst = ch_out(['git', 'log', '--oneline']).splitlines()
+length = len(lst)
+
 while True:
-    cent = (n[0] + n[1]) // 2
-    if lst[cent] != x:
-        if abs(n[0]-n[1]) == 1:
-            n = -1
-            break
-        if lst[cent] > x:
-            n[1] = cent
-        else:
-            n[0] = cent
+    cent = length // 2
+    if auto:
+        system(f'git checkout {lst[cent][:6]}')
+        chk = system(a_command)
     else:
-        n = cent
-        break
-print(n)
-# WIP!!!!
+        try:
+            chk = int(input(f'Check {lst[cent]}. Bad (0) or good (1 or else) commit? '))
+        except ValueError:
+            print('NaN!')
+            exit(1)
+    if chk:
+        lst = lst[:cent]
+    else:
+        lst = lst[cent:]
+    length = len(lst)
+    if len(lst) == 1:
+        print(f'{lst[0]} is bad!')
+        exit(0)
