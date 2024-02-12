94f7423cfd0526a61c8d3b6fa16a248e89e72a3e
Ikadzev <ikadzev@mail.ru>
2024-02-12 22:31:25 +0700

    mini 4 DONE

--- minies/4.py
+++ minies/4.py
@@ -1,26 +1,31 @@
-from subprocess import check_output as ch_out
+from subprocess import run, check_output as ch_out
 from os import chdir, system, getcwd as pwd
 
-iwd = pwd()
 wd = input('WD: ')
 if wd == '':
-    wd = iwd
+    wd = pwd()
 b = input('Bad commit: ')
 g = input('Good commit: ')
 auto = True if int(input('Manual (0) or auto (1)? ')) else False
 a_command = input('Command for auto mode: ') if auto else None
 chdir(wd)
-lst = ch_out(['git', 'log', '--oneline']).splitlines()
+if b != '':
+    lst = ch_out(['git', 'log', '--oneline', f'{g}..{b}']).decode().splitlines()
+else:
+    lst = ch_out(['git', 'log', '--oneline']).decode().splitlines()
+# lst = ch_out(['git', 'log', '--oneline', f'{g}..{b}' if b or g else '']).decode().splitlines()
+
 length = len(lst)
 
 while True:
     cent = length // 2
     if auto:
         system(f'git checkout {lst[cent][:6]}')
-        chk = system(a_command)
+        chk = run(a_command, capture_output=False).returncode
+        system('git checkout -')
     else:
         try:
-            chk = int(input(f'Check {lst[cent]}. Bad (0) or good (1 or else) commit? '))
+            chk = int(input(f'Check "{lst[cent]}". Bad (0) or good (1 or else) commit? '))
         except ValueError:
             print('NaN!')
             exit(1)
