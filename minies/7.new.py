01d70571bc3899402376b308e8a67eba5a6d10c7
Ikadzev <ikadzev@mail.ru>
2024-02-23 17:55:08 +0700

    mini 7 cleaning

--- minies/7.new.py
+++ minies/7.new.py
@@ -8,23 +8,6 @@ def swap(lst, f, s):
 
 
 def merge(lst, f, len_f, s, len_s, buff):
-    # b = []
-    # i, j = 0, 0
-    # while i < len(f) and j < len(s):
-    #     if f[i] < s[j]:
-    #         b.append(f[i])
-    #         i += 1
-    #     else:
-    #         b.append(s[j])
-    #         j += 1
-    # while i < len(f):
-    #     b.append(f[i])
-    #     i += 1
-    # while j < len(s):
-    #     b.append(s[j])
-    #     j += 1
-    # print('returning after merge, b =', b) if DEBUG_RETURN else None
-    # return b
     assert len(lst) - buff >= len_f + len_s
     while len_f and len_s:
         if lst[f] < lst[s]:
@@ -50,27 +33,6 @@ def merge(lst, f, len_f, s, len_s, buff):
     return lst
 
 
-def old_sort(lst):
-    n = len(lst)
-    if n == 1:
-        print('returning after n=1, lst =', lst) if DEBUG_RETURN else None
-        return lst
-    if n == 2:
-        if lst[0] > lst[1]:
-            swap(lst, 0, 1)
-        print('returning after n=2, lst=', lst) if DEBUG_RETURN else None
-        return lst
-
-    cnt = n // 2
-    print('sorting f, lst =', lst, 'cnt =', cnt) if DEBUG_SORT else None
-    f = sort(lst[:cnt])
-    print('sorting s, lst =', lst, 'cnt =', cnt) if DEBUG_SORT else None
-    s = sort(lst[cnt:])
-    print('merging, f =', f, 's =', s, 'cnt =', cnt) if DEBUG_MERGE else None
-    lst = merge(f, s)
-    return lst
-
-
 def sort(lst, s=0, e=None):
     e = e if e else len(lst)
     n = e - s
@@ -80,12 +42,6 @@ def sort(lst, s=0, e=None):
     if n == 2:
         swap(lst, s, e - 1) if lst[s] > lst[e - 1] else None
         return lst
-    # if n == 3:
-    #     for i in range(3):
-    #         for j in range(i):
-    #             if lst[i] < lst[j]:
-    #                 swap(lst, i, j)
-    #     return lst
 
     cent = n // 2
     lst = sort(lst, 0, cent // 2)
@@ -108,8 +64,7 @@ def sort(lst, s=0, e=None):
     return lst
 
 
-numbers = '24 11 32 15 6 14 17 9' # 36 16 18 33 5 10 29 25'
-# numbers = '8 7 6 5 4 3 2 1'
+numbers = '24 11 32 15 6 14 17 9'  # 36 16 18 33 5 10 29 25'
 str_list = numbers.split()
 int_list = [int(i) for i in str_list]
 print(sort(int_list))
