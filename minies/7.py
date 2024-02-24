6a476d7e111709614c39f4e8b11fdf07b62debac
Ikadzev <ikadzev@mail.ru>
2024-02-24 10:56:10 +0700

    mini 7 DONE

--- minies/7.py
+++ minies/7.py
@@ -7,48 +7,66 @@ def swap(lst, f, s):
     lst[f], lst[s] = lst[s], lst[f]
 
 
-def merge(f, s):
-    b = []
-    i, j = 0, 0
-    while i < len(f) and j < len(s):
-        if f[i] < s[j]:
-            b.append(f[i])
-            i += 1
+def merge(lst, f, lf, s, ls, buff):
+
+    while f < lf and s < ls:
+        if lst[f] < lst[s]:
+            swap(lst, f, buff)
+            f += 1
         else:
-            b.append(s[j])
-            j += 1
-    while i < len(f):
-        b.append(f[i])
-        i += 1
-    while j < len(s):
-        b.append(s[j])
-        j += 1
-    print('returning after merge, b =', b) if DEBUG_RETURN else None
-    return b
-
-
-def sort(lst):
-    n = len(lst)
-    if n == 1:
-        print('returning after n=1, lst =', lst) if DEBUG_RETURN else None
-        return lst
-    if n == 2:
-        if lst[0] > lst[1]:
-            swap(lst, 0, 1)
-        print('returning after n=2, lst=', lst) if DEBUG_RETURN else None
-        return lst
-
-    cnt = n // 2
-    print('sorting f, lst =', lst, 'cnt =', cnt) if DEBUG_SORT else None
-    f = sort(lst[:cnt])
-    print('sorting s, lst =', lst, 'cnt =', cnt) if DEBUG_SORT else None
-    s = sort(lst[cnt:])
-    print('merging, f =', f, 's =', s, 'cnt =', cnt) if DEBUG_MERGE else None
-    lst = merge(f, s)
+            swap(lst, s, buff)
+            s += 1
+        buff += 1
+    while f < lf:
+        swap(lst, f, buff)
+        f += 1
+        buff += 1
+    while s < ls:
+        swap(lst, s, buff)
+        s += 1
+        buff += 1
     return lst
 
 
-numbers = '20 1 28 26 14 29 37 7 10 35 5 11 15 25 22 40'
+def sort(lst, s=0, e=-1):
+    e = e if e != -1 else len(lst)
+    n = e - s
+    assert n != 0
+    if n == 1:
+        return lst
+    if n < 4:
+        for i in range(e - 2, s - 1, -1):
+            if lst[i] > lst[i + 1]:
+                t = i
+                while lst[t] > lst[t + 1] and t < e:
+                    swap(lst, t, t+1)
+        return lst
+
+    start = s
+    s += n % 4
+    cent = (s + e) // 2
+    quat = (s + cent) // 2
+
+    lst = sort(lst, s, quat)
+    lst = sort(lst, quat, cent)
+    lst = merge(lst, s, quat, quat, cent, cent)
+
+    ln = [quat, cent]
+    while ln[0] - s > 1:
+        lst = sort(lst, s, ln[0])
+        lst = merge(lst, s, ln[0], ln[1], e, ln[0])
+        ln[1] = ln[0] + (ln[0] - s) % 2
+        ln[0] = (s + ln[1]) // 2
+
+    for i in range(ln[1] - 1, start - 1, -1):
+        t = i
+        while t < e - 1 and lst[t] > lst[t + 1]:
+            swap(lst, t, t + 1)
+            t += 1
+    return lst
+
+
+numbers = '24 11 15 32 6 14 17 36 16 18 33 5 10 29 25'
 str_list = numbers.split()
 int_list = [int(i) for i in str_list]
 print(sort(int_list))
