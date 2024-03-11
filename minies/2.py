2cf766da20047d317af4b7cb542f85971675e232
ikadzev <ikadzev@mail.ru>
2024-03-11 13:55:00 +0700

    mini 2 FIX

--- minies/2.py
+++ minies/2.py
@@ -1,23 +1,16 @@
 def algo(a, b):
+    length = max(len(str(a)), len(str(b)))
+    length += length % 2
+    hlen = length // 2
     
-    a, b = str(a), str(b)
-    length = max(len(a), len(b))
-    if length % 2:
-        length += 1
-    hlen = int(length * 0.5)
-    if len(a) != length:
-        a = '0' * (length - len(a)) + a
-    if len(b) != length:
-        b = '0' * (length - len(b)) + b
-    
-    aa = a[:hlen]
-    bb = a[hlen:]
-    cc = b[:hlen]
-    dd = b[hlen:]
-    f = int(aa)*int(cc) if len(aa) == 1 and len(cc) == 1 else algo(aa, cc)
-    s = ((int(aa)+int(bb)) * (int(cc) + int(dd))) if len(str(int(aa)+int(bb))) == 1 and len(str(int(cc) + int(dd))) == 1 else algo(int(aa)+int(bb), int(cc) + int(dd))
-    t = int(bb)*int(dd) if len(bb) == 1 and len(dd) == 1 else algo(bb, dd)
-    
-    return (10**length*int(aa)*int(cc) + 10**hlen*(int(aa)*int(dd) + int(bb)*int(cc)) + int(bb)*int(dd))
+    aa = a // 10**hlen
+    bb = a % 10**hlen
+    cc = b // 10**hlen
+    dd = b % 10**hlen
+    f = aa*cc if aa // 10 == 0 and cc // 10 == 0 else algo(aa, cc)
+    s = ((aa + bb) * (cc + dd)) if (aa + bb) // 10 == 0 and (cc + dd) // 10 == 0 else algo(aa + bb, cc + dd)
+    t = bb*dd if bb // 10 == 0 and dd // 10 == 0 else algo(bb, dd)
 
-print(algo(12, 50))
+    return (10**length * f + 10**hlen * (s - t - f) + t)
+
+print(algo(348, 574))
