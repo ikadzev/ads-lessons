b59682915b5b1abf5f82f1a00f661251bd9a54d3
ikadzev <ikadzev@mail.ru>
2024-03-15 12:11:48 +0700

    mini 18 FIX

--- minies/18.py
+++ minies/18.py
@@ -14,14 +14,14 @@ def pol(prompt):
             while cnt_brk[-1] != 0:
                 ret.append(stk.pop())
                 cnt_brk[-1] -= 1
-            cnt_brk = cnt_brk[-1]
+            cnt_brk = cnt_brk[:-1]
         else:
             if stk:
                 if cnt_brk and cnt_brk[-1] == 0:
                     stk.append(lex)
                     cnt_brk[-1] += 1
                     continue
-                while priority[lex] > priority[stk[-1]]:
+                while stk and priority[lex] > priority[stk[-1]]:
                     ret.append(stk.pop())
                     if cnt_brk:
                         cnt_brk[-1] -= 1
