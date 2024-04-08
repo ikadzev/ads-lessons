9d2eb6f59cc01c5776d26a2909a9058aff5a51d8
Valentin <ikadzev@mail.ru>
2024-04-08 13:15:00 +0700

    mini 14 FIX

--- minies/14.py
+++ minies/14.py
@@ -1,48 +1,38 @@
 from random import randint
-
-
-def move(array, index, place):
-    if index == place:
-        return
-    c = array[index]
-    step = -1 if place < index else 1
-    for i in range(index, place, step):
-        array[i] = array[i + step]
-    array[place] = c
+from funcs import flag
 
 
 def swap(array, f, s):
     array[f], array[s] = array[s], array[f]
 
 
-def fast_sort(array, pivot):
-    indexs = [0, 1, 1]
-    swap(array, pivot, 0)
-    pivot = array[0]
-    n = 1
-    while n != len(array):
-        if array[n] < pivot:
-            index = 0
-        elif array[n] == pivot:
-            index = 1
+def quick_sort(array, pivot):
+    length = len(array)
+    i = 0
+    colors = [0, length - 1]
+    while i < length and i <= colors[1]:
+        if array[i] < pivot:
+            if i != colors[0]:
+                swap(array, i, colors[0])
+            colors[0] += 1
+            swappable = colors[0] - 1
+        elif array[i] == pivot:
+            swappable = i
         else:
-            index = 2
-        move(array, n, indexs[index])
-        indexs[index] += 1
-        if index <= 1:
-            indexs[2] += 1
-            if index == 0:
-                indexs[1] += 1
-        n += 1
-    return indexs[0]
+            if i != colors[1]:
+                swap(array, i, colors[1])
+            colors[1] -= 1
+            swappable = colors[1] + 1
+        if swappable == i:
+            i += 1
+    return array, colors
 
 
 def kth(array, k):
     if len(array) == 1:
         return array[0]
     pivot = randint(0, len(array) - 1)
-    p = fast_sort(array, pivot)
-
+    p = flag(array, array[pivot])[1][0]
     if p == k:
         return array[p]
     elif p > k:
@@ -52,9 +42,17 @@ def kth(array, k):
 
 
 def oil(array):
-    array = [i[1] for i in array]
     return kth(array, len(array) // 2)
 
 
-oil_list = [[1, 2], [4, 7], [3, 8], [2, 1], [5, 3]]
-print(oil(oil_list))
+oil_list = [2, 7, 8, 1, 3]
+print(oil(oil_list) == sorted(oil_list)[len(oil_list) // 2])
+
+oil_list = [66, 12, 24, 23, 67, 41, 26, 30, 57, 68]
+print(oil(oil_list) == sorted(oil_list)[len(oil_list) // 2])
+
+oil_list = [84, 86, 60, 96, 16, 42, 16, 25, 49, 21, 97, 6, 20, 95, 39, 15, 76, 61, 42, 76, 54, 78, 23, 63, 41, 83, 4, 8,
+            16, 53, 25, 5, 41, 99, 81, 24, 5, 45, 26, 29, 26, 91, 87, 90, 84, 11, 56, 22, 43, 68, 39, 91, 39, 99, 59,
+            75, 68, 61, 64, 15, 94, 47, 39, 99, 15, 42, 78, 43, 67, 48, 80, 97, 44, 69, 55, 97, 6, 11, 76, 52, 64, 25,
+            90, 30, 69, 9, 44, 89, 59, 83, 31, 19, 31, 80, 39, 32, 64, 21, 74, 55]
+print(oil(oil_list) == sorted(oil_list)[len(oil_list) // 2])
