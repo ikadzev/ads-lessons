bf1ce6f261fc331bf53c624b045a3f548cd93a5f
Valentin <ikadzev@mail.ru>
2024-05-24 11:52:57 +0700

    31 FIX

--- minies/31.py
+++ minies/31.py
@@ -1,7 +1,10 @@
 class Node:
-    def __init__(self, value, kids=()):
+    def __init__(self, value, kids=None):
         self.value = value
-        self.kids = kids
+        self.kids = kids if kids else []
+
+    def __str__(self):
+        return f'Node {self.value} with kids {[i.value for i in self.kids]}'
 
 
 def rec_comp(nodes: dict) -> list[Node]:
@@ -9,23 +12,23 @@ def rec_comp(nodes: dict) -> list[Node]:
     visited = len(nodes_linked.keys())
     depens = []
     for node in nodes_linked.values():
-        for kid in node.kids():
+        for kid in node.kids:
             depens.append((node.value, kid.value))
-    depens = [(dep[1], dep[0]) for dep in depens]
     reversed_nodes = {name: Node(name) for name in nodes_linked.keys()}
     for depen in depens:
-        reversed_nodes[depen[0]].kids.append(depen[1])
+        reversed_nodes[depen[1]].kids.append(reversed_nodes[depen[0]])
     visited, stack = [], []
     for node in reversed_nodes.values():
         visited, stack = dfs(node, visited, stack)
     comps = []
+    visited = []
+    comps = []
     while stack:
         node = stack.pop()
-        visited = []
-        comps = []
         comp = []
-        visited, comp = dfs_comps(node, visited, comp)
+        visited, comp = dfs_comps(nodes_linked[node.value], visited, comp)
         comps.append(comp)
+    print(comps)
     mx = []
     for i in comps:
         if len(mx) < len(i):
@@ -42,7 +45,7 @@ def dict2link(nodes: dict) -> dict:
             depens.append((node, kid))
     nodes = {name: Node(name) for name in names}
     for depen in depens:
-        nodes[depen[0]].kids.append(depen[1])
+        nodes[depen[0]].kids.append(nodes[depen[1]])
     return nodes
 
 
@@ -50,8 +53,8 @@ def dfs_comps(node: Node, visited: list[Node], comp: list[Node]) -> (list[Node],
     if node not in visited:
         comp.append(node)
         visited.append(node)
-        for kid in node.kids():
-            visited, comp = dfs_comps(node, visited, comp)
+        for kid in node.kids:
+            visited, comp = dfs_comps(kid, visited, comp)
     return visited, comp
 
 
@@ -62,3 +65,15 @@ def dfs(node, visited, stk):
             visited, stk = dfs(kid, visited, stk)
         stk.append(node)
     return visited, stk
+
+
+funcs = {'0': ['4'],
+         '1': ['0', '7'],
+         '2': ['1', '4', '6'],
+         '3': ['0', '3'],
+         '4': ['0'],
+         '5': ['2'],
+         '6': ['4', '5'],
+         '7': ['1', '3']}
+
+print([i.value for i in rec_comp(funcs)])
