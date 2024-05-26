505a9188d394844fd45c5f0befb411a087d1f51f
Valentin <ikadzev@mail.ru>
2024-05-26 13:25:48 +0700

    31 FIX

--- minies/31.py
+++ minies/31.py
@@ -7,33 +7,49 @@ class Node:
         return f'Node {self.value} with kids {[i.value for i in self.kids]}'
 
 
-def rec_comp(nodes: dict) -> list[Node]:
+def rec_comp(nodes: dict) -> (list[Node], dict[str, bool]):
     nodes_linked = dict2link(nodes)
-    visited = len(nodes_linked.keys())
+    is_recs = nodes_linked.values()
+    is_recs = {func.value: is_rec(func) for func in is_recs}
     depens = []
     for node in nodes_linked.values():
         for kid in node.kids:
             depens.append((node.value, kid.value))
+
     reversed_nodes = {name: Node(name) for name in nodes_linked.keys()}
     for depen in depens:
         reversed_nodes[depen[1]].kids.append(reversed_nodes[depen[0]])
+
     visited, stack = [], []
     for node in reversed_nodes.values():
         visited, stack = dfs(node, visited, stack)
-    comps = []
+
     visited = []
     comps = []
     while stack:
         node = stack.pop()
         comp = []
         visited, comp = dfs_comps(nodes_linked[node.value], visited, comp)
-        comps.append(comp)
-    print(comps)
-    mx = []
-    for i in comps:
-        if len(mx) < len(i):
-            mx = i
-    return mx
+        comps.append(comp) if comp else None
+    for comp in comps:
+        for node in range(len(comp)):
+            comp[node] = comp[node].value
+    return comps, is_recs
+
+
+def is_rec(node):
+    def _is_rec_inner(node, init):
+        if init in node.kids:
+            return True
+        for kiddo in node.kids:
+            if _is_rec_inner(kiddo, init):
+                return True
+        return False
+
+    for kid in node.kids:
+        if _is_rec_inner(kid, node):
+            return True
+    return False
 
 
 def dict2link(nodes: dict) -> dict:
@@ -67,13 +83,21 @@ def dfs(node, visited, stk):
     return visited, stk
 
 
-funcs = {'0': ['4'],
-         '1': ['0', '7'],
-         '2': ['1', '4', '6'],
-         '3': ['0', '3'],
-         '4': ['0'],
-         '5': ['2'],
-         '6': ['4', '5'],
-         '7': ['1', '3']}
-
-print([i.value for i in rec_comp(funcs)])
+# funcs = {'0': ['4'],
+#          '1': ['0', '7'],
+#          '2': ['1', '4', '6'],
+#          '3': ['0', '3'],
+#          '4': ['0'],
+#          '5': ['2'],
+#          '6': ['4', '5'],
+#          '7': ['1', '3']}
+funcs = {'foo': ['bar', 'baz', 'qux'],
+         'bar': ['baz', 'foo', 'bar'],
+         'qux': ['qux'],
+         'baz': []}
+cycles, recs = rec_comp(funcs)
+mx = max([len(i) for i in cycles])
+maax = [i for i in cycles if len(i) == mx]
+print(*maax, sep=' ')
+rcs = [f'{i} is recursive!' for i, j in recs.items() if j]
+print(*rcs, sep='\n')
